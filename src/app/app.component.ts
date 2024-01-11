c14615f2b844d306a93f20619ef14a9036e00a08
Jake Zeledon <jackiezeledon@gmail.com>
2024-01-10 19:01:30 -0600

    Checking right loading of theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { SplitterModule } from 'primeng/splitter';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule, CardModule, SplitterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
