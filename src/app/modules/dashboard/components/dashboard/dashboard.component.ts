587c25714beeed5b8175bd99d1b2e87f0f3cfe7e
Jake Zeledon <jackiezeledon@gmail.com>
2024-01-12 01:51:13 -0600

    Add dashboard

--- src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from '../../../product/components/product/product.component'; 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ RouterOutlet, ProductComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
