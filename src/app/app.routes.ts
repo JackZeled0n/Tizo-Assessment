3871a2e50a95cc82ad5dce7fd1566af00512e27b
Jake Zeledon <jackiezeledon@gmail.com>
2024-01-11 23:14:44 -0600

    update project architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () =>
+            import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
+    },
+    {
+        path: 'user',
+        loadChildren: () =>
+            import('./modules/user/user.module').then((m) => m.UserModule),
+    },
+    {
+        path: 'product',
+        loadChildren: () =>
+            import('./modules/product/product.module').then((m) => m.ProductModule),
+    },
+    {
+        path: 'category',
+        loadChildren: () =>
+            import('./modules/category/category.module').then((m) => m.CategoryModule),
+    },
+];
