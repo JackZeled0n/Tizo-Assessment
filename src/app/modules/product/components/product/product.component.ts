9982474423cf247125a283dffabbc591917fed2f
Jake Zeledon <jackiezeledon@gmail.com>
2024-01-12 16:50:15 -0600

    Add search input functionality

--- src/app/modules/product/components/product/product.component.ts
+++ src/app/modules/product/components/product/product.component.ts
@@ -12,7 +12,7 @@ import { IProduct } from '../../../../core/interfaces/products';
   styleUrl: './product.component.css'
 })
 export class ProductComponent implements OnInit {
-
+  filteredProducts: IProduct[] = [];
 
   constructor(
     private productService: ProductService
@@ -29,13 +29,14 @@ export class ProductComponent implements OnInit {
     .getProducts()
     .subscribe((data: IProduct[]) => {
       this.productsArray = data;
+      this.filteredProducts = data;
     });
   }
 
   updateStatusProduct(id: number): void {
     const product = this.productsArray.find(p => p.id === id);
     if (product) {
-      product.active = false;
+      product.active = product.active ? false : true;
 
       this.productService.updateProduct(product).subscribe(() => {
         console.log('Campo actualizado correctamente');
@@ -43,4 +44,17 @@ export class ProductComponent implements OnInit {
     }
   }
 
+  onSearch(searchTerm: string | undefined) {
+    if (searchTerm === undefined) {
+      this.filteredProducts = this.productsArray;
+    } else {
+      this.filteredProducts = this.productsArray.filter(product =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+  }
+
+  getValue(event: Event): string {
+    return (event.target as HTMLInputElement).value;
+  }
 }
