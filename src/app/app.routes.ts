588907f84a156d4c8e0794d7f442bfe5539c4f5a
Jake Zeledon <jackiezeledon@gmail.com>
2024-01-11 23:20:09 -0600

    add authentication module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ export const routes: Routes = [
     {
         path: '',
         loadChildren: () =>
+            import('./modules/authentication/authentication.module').then((m) => m.AuthenticationModule),
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () =>
             import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
     },
     {
